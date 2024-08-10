0329840d85b81a19b51a5872aee24601443626ca
ksd <kimseong124@naver.com>
2024-08-10 10:12:33 +0900

    FPS setting

--- maingame.py
+++ maingame.py
@@ -12,37 +12,45 @@ screen = pygame.display.set_mode(screen_size)
 # 화면 타이틀 설정
 pygame.display.set_caption("Pygame")
 
+# FPS
+clock = pygame.time.Clock()
+
 # 플레이어 캐릭터 불러오기
 player = pygame.image.load("images/player.png").convert_alpha() # 플레이어 캐릭터 이미지 불러오기
 player_size = player.get_rect().size # 이미지 크기 구하기
 player_width = player_size[0] # 플레이어 가로 크기
 player_height = player_size[1] # 플레이어 세로 크기
+
 # 화면 중앙에 플레이어 위치
 player_x_pos = (screen_width / 2) - (player_width / 2)
 player_y_pos = (screen_height / 2) - (player_height / 2)
 
-# 이동할 좌표
+# 플레이어가 이동할 좌표
 to_x = 0
 to_y = 0
-MOVE_DIST = 2 # 한 번에 얼만큼 움직일 것인가?
+
+# 플레이어 이동 속도
+player_speed = 0.2
 
 
 # 이벤트 루프
 running = True # 게임이 진행중인가?
 while running:
+    dt = clock.tick(60) # FPS 설정 (1초당 60번 동작)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # 창을 닫으면 게임 종료
             running = False
 
         if event.type == pygame.KEYDOWN: # 방향키를 눌렀을 시
             if event.key == pygame.K_LEFT: 
-                to_x -= MOVE_DIST
+                to_x -= player_speed
             if event.key == pygame.K_RIGHT:
-                to_x += MOVE_DIST
+                to_x += player_speed
             if event.key == pygame.K_UP:
-                to_y -= MOVE_DIST
+                to_y -= player_speed
             if event.key == pygame.K_DOWN:
-                to_y += MOVE_DIST
+                to_y += player_speed
 
         if event.type == pygame.KEYUP: # 방향키에서 뗐을 시
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -51,8 +59,8 @@ while running:
                 to_y = 0
             
     # 플레이어 좌표 계산
-    player_x_pos += to_x
-    player_y_pos += to_y
+    player_x_pos += to_x * dt
+    player_y_pos += to_y * dt
 
     # 가로 좌표 경계 계산
     if player_x_pos < 0:
