1e22063d3034458ab6485545f2d50d4bdc6a0646
ksd <kimseong124@naver.com>
2024-08-10 10:40:07 +0900

    add enemy, background

--- maingame.py
+++ maingame.py
@@ -15,6 +15,9 @@ pygame.display.set_caption("Pygame")
 # FPS
 clock = pygame.time.Clock()
 
+# 배경화면 설정
+background = pygame.image.load("images/background.png").convert_alpha()
+
 # 플레이어 캐릭터 불러오기
 player = pygame.image.load("images/player.png").convert_alpha() # 플레이어 캐릭터 이미지 불러오기
 player_size = player.get_rect().size # 이미지 크기 구하기
@@ -25,13 +28,22 @@ player_height = player_size[1] # 플레이어 세로 크기
 player_x_pos = (screen_width / 2) - (player_width / 2)
 player_y_pos = (screen_height / 2) - (player_height / 2)
 
+# 적 캐릭터 불러오기
+enemy = pygame.image.load("images/enemy.png").convert_alpha() # 적 캐릭터 이미지 불러오기
+enemy_size = enemy.get_rect().size # 이미지 크기 구하기
+enemy_width = enemy_size[0] # 적 가로 크기
+enemy_height = enemy_size[1] # 적 세로 크기
+
+# 임의의 위치에 적 위치
+enemy_x_pos = (screen_width / 2) - (enemy_width / 2)
+enemy_y_pos = screen_height - enemy_height
+
 # 플레이어가 이동할 좌표
 to_x = 0
 to_y = 0
 
 # 플레이어 이동 속도
 player_speed = 0.2
-
 
 # 이벤트 루프
 running = True # 게임이 진행중인가?
@@ -74,8 +86,9 @@ while running:
     elif player_y_pos > screen_height - player_height:
         player_y_pos = screen_height - player_height
     
-    screen.fill((255, 255, 255)) # 배경화면 (임시)
+    screen.blit(background, (0, 0)) # 배경화면
     screen.blit(player, (player_x_pos, player_y_pos)) # 플레이어 그리기
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos)) # 적 그리기
     pygame.display.update() # 게임화면 다시 그리기
 
 # pygame 종료
